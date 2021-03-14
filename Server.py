0cb40fbf9c91d9f0bf1058e43fde563d29b42868
Madswolf <mads@madswolf.dk>
2021-03-14 01:30:19 +0000

    added zigbee light support

--- Server.py
+++ Server.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import lirc
+from platypush.context import get_plugin
 
 app = Flask(__name__)
 client = lirc.Client()
+
+#mqtt_client = mqtt.Client("P1") #create new instance
+#mqtt_client.connect("localhost",1883,60) #connect to broker
 
 projector_remote = "AAXA_P7"
 soundbar_remote = "AH59-02767A"
@@ -15,6 +19,7 @@ projector_keys = {
     "menuDown": "DOWN",
     "menuLeft": "LEFT",
     "menuRight": "RIGHT",
+    "menuOK": "OK",
     "inputMenu":"INPUT_SELECT",
     "keystoneUp": "KEYSTONE_UP",
     "keystoneDown": "KEYSTONE_DOWN",
@@ -23,6 +28,7 @@ projector_keys = {
 
 soundbar_keys = {
     "power":"POWER",
+    "play-pause": "PLAY_PAUSE",
     "inputSwitch": "INPUT_SWITCH",
     "bluetoothPair": "BT_PAIR",
     "EQSwitch": "SOUND_MODE",
@@ -35,6 +41,10 @@ soundbar_keys = {
     "bassDown": "BASS_DOWN",
     "bassToZero": "BASS_TOZERO",
 }
+
+@app.route('/')
+def index():
+    return render_template('index.html', projector_keys = projector_keys, soundbar_keys = soundbar_keys)
 
 @app.route('/Projector/<key>')
 def projector_send_key(key):
@@ -50,6 +60,29 @@ def soundbar_send_key(key):
         return "Success"    
     return "that not very poggies of you"
 
+
+@app.route('/Lys/brightness/<value>')
+def lys_set(value):
+    try:
+        value = int(value)
+        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness', value=str(value))
+        return "Success"
+    except:
+        return "that not very poggies of you"
+
+@app.route('/Lys/brightness/step/<value>')
+def lys_step(value):
+    try:
+        value = int(value)
+        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness_move', value=str(value))
+        return "Success"
+    except:
+        return "that not very poggies of you"
+
+@app.route('/Lys/')
+def lys_():
+    get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='state', value='TOGGLE')
+    return "Success"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port = 8000,debug = True)
