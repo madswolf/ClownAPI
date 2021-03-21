eb73d20cfc5e162bec363f5117c230696259be61
Madswolf <mads@madswolf.dk>
2021-03-21 23:58:25 +0000

    added recaptcha to prevent botting

--- Server.py
+++ Server.py
@@ -3,6 +3,8 @@ import lirc
 from platypush.context import get_plugin
 from dotenv import load_dotenv
 import os
+from flask_wtf import Form, RecaptchaField
+from wtforms import SubmitField
 load_dotenv()
 
 app = Flask(__name__)
@@ -49,7 +51,34 @@ ir_devices = {
 
 
 api_key = os.environ.get("api_key")
+SECRET_KEY = '78w0o5tuuGex5Ktk8VvVDF9Pw3jv1MVE'
+recaptcha_public_key = os.environ.get("recaptcha_public")
+recaptcha_private_key = os.environ.get("recaptcha_private")
 app.locked = False
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['RECAPTCHA_USE_SSL']= False
+app.config['RECAPTCHA_PUBLIC_KEY']= recaptcha_public_key
+app.config['RECAPTCHA_PRIVATE_KEY']= recaptcha_private_key
+app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
+
+def isAllowed():
+    return request.headers.get('api_key',"",type=str) != api_key and app.locked
+
+def isNotBot(form):
+    return (request.method == 'POST' and form.validate()) or request.headers.get('api_key',"",type=str) == api_key
+
+class AllowedForm(Form):
+        recaptcha = RecaptchaField()
+        submit = SubmitField('Submit')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = AllowedForm(request.form)
+    if request.method == 'POST' and form.validate():
+        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='state', value='TOGGLE')
+        return 'Success'
+    return render_template('default.html', form=form, endpoint="register")
 
 @app.route('/')
 def index():
@@ -57,56 +86,71 @@ def index():
 
 @app.route('/lock', methods=['POST'])
 def lock():
-    if (api_key == request.form['api_key']):
+    if (request.headers.get('api_key',"",type=str) == api_key):
         app.locked = not app.locked
     return "locked" if app.locked else "unlocked"
 
-@app.route('/<device>/<key>')
+@app.route('/<device>/<key>', methods=['GET','POST'])
 def projector_send_key(device,key):
-    if(request.form.get('api_key',"",type=str) != api_key and app.locked):
+    if(isAllowed()):
         return "Not now kiddo"
 
-    device = device.lower()
-    key = key.lower()
+    form = AllowedForm(request.form)
+    if isNotBot(form):
+        device = device.lower()
+        key = key.lower()
 
-    if(device in ir_devices.keys()):
-        commands,remote = ir_devices[device]
-        if(key in commands.keys()):
-            client.send_once(remote,commands[key],repeat_count=0)
-        return "Success"    
-    return "that not very poggies of you"
+        if(device in ir_devices.keys()):
+            commands,remote = ir_devices[device]
+            if(key in commands.keys()):
+                client.send_once(remote,commands[key],repeat_count=0)
+            return "Success"    
+        return "that not very poggies of you"
 
-@app.route('/Lys/brightness/<value>')
+    return render_template('default.html', form=form, url='http://clown.mads.monster/{}/{}'.format(device,key))
+
+@app.route('/Lys/brightness/<value>', methods=['GET','POST'])
 def lys_set(value):
-    if(request.form.get('api_key',"",type=str) != api_key and app.locked) :
+    if(isAllowed()):
         return "Not now kiddo"
 
-    try:
-        value = int(value)
-        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness', value=str(value))
-        return "Success"
-    except:
-        return "that not very poggies of you"
+    form = AllowedForm(request.form)
+    if isNotBot(form):
+        try:
+            value = int(value)
+            get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness', value=str(value))
+            return "Success"
+        except:
+            return "that not very poggies of you"
 
-@app.route('/Lys/brightness/step/<value>')
+    return render_template('default.html', form=form, url='http://clown.mads.monster/Lys/brightness/{}'.format(value))
+
+@app.route('/Lys/brightness/step/<value>', methods=['GET','POST'])
 def lys_step(value):
-    if(request.form.get('api_key',"",type=str) != api_key and app.locked):
+    if(isAllowed()):
         return "Not now kiddo"
 
-    try:
-        value = int(value)
-        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness_move', value=str(value))
-        return "Success"
-    except:
-        return "that not very poggies of you"
+    form = AllowedForm(request.form)
+    if isNotBot(form):
+        try:
+            value = int(value)
+            get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='brightness_move', value=str(value))
+            return "Success"
+        except:
+            return "that not very poggies of you"
 
-@app.route('/Lys')
+    return render_template('default.html', form=form, url='http://clown.mads.monster/Lys/brightness/step/{}'.format(value))
+
+@app.route('/Lys', methods=['GET','POST'])
 def lys_():
-    if(request.form.get('api_key',"",type=str) != api_key and app.locked):
+    if(isAllowed()):
         return "Not now kiddo"
-
-    get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='state', value='TOGGLE')
-    return "Success"
+    form = AllowedForm(request.form)
+    if isNotBot(form):
+        get_plugin('zigbee.mqtt').group_set(group='stue-lys', property='state', value='TOGGLE')
+        return "Success"
+        
+    return render_template('default.html', form=form, url='http://clown.mads.monster/Lys')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port = 8000,debug = True)
